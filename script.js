1e3e64e172be6c441dc15031add7db3b3f539411
Randy-Stith <rst6919@gmail.com>
2025-01-26 18:13:06 -0500

    added the first screen and some sound

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
+
+
+
+
     //Sending to portfolio after click 
     $(document).on('click keypress', () => {
-        window.location.href = 'homepage.html'
-    })
+        //audio
+       if (document.querySelector('#audioprompt') == null) 
+        {
+                document.body.innerHTML = 
+            '<audio id="audioprompt" autoplay><source src="audio/game-start-6104.mp3"></audio>'
+             + document.body.innerHTML;
+
+           } 
+    function next() 
+    {window.location.href='homepage.html'}
+    setTimeout(next, 2000)
+   
+})
+
+
+
+
   });
 
 
